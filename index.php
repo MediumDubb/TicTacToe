0a57c1d28173d74483b722482c1aa8e202849c49
MediumDubb <noah.whittington.1997@gmail.com>
2022-09-03 21:48:09 -0400

    init commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Happy Birthday Dad</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="menu">
+            <div class="menu-wrapper">
+                <table>
+                    <tr>
+                        <th>P1: score</th>
+                        <th>P2: score</th>
+                        <th>Winner</th>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <div class="board-wrapper">
+            <table class="board">
+                <tr>
+                    <td>a</td>
+                    <td>s</td>
+                    <td>c</td>
+                </tr>
+                <tr>
+                    <td>f</td>
+                    <td>g</td>
+                    <td>h</td>
+                </tr>
+                <tr>
+                    <td>j</td>
+                    <td>k</td>
+                    <td>l</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
+<?php
+echo "Hello World";
+?>
+</body>
+</html>
