7cac94b295e91fa2c9956b389fce82d417c97079
MediumDubb <noah.whittington.1997@gmail.com>
2022-09-05 23:08:40 -0400

    minor tweaks

--- index.php
+++ index.php
@@ -9,6 +9,26 @@
     <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 </head>
 <body>
+<?php
+    try {
+        $dbh = new PDO('mysql:host=localhost;dbname=tictactoe', 'root', '');
+        foreach($dbh->query('SELECT * from gameroom') as $row) {
+            print_r($row);
+        }
+        $dbh = null;
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
+    }
+
+    if( ! isset($_GET['roomID']) && ! isset($_GET['playerID'])){
+// check for unfilled room (either join one with someone waiting or make a new room)
+// assign room id
+// assign player id
+// assign tictactoe character (x/o)
+
+    }
+?>
 <div class="container">
     <div class="row">
         <div class="menu-wrapper">
@@ -21,24 +41,34 @@
             </table>
         </div>
         <div class="board-wrapper">
-            <form action="" method="POST">
+            <form action="index.php" method="POST">
                 <table class="board">
-                    <tr>
-                        <td><input type="hidden" id="a1"></td>
-                        <td><input type="hidden" id="a2"></td>
-                        <td><input type="hidden" id="a3"></td>
-                    </tr>
-                    <tr>
-                        <td><input type="hidden" id="b1"></td>
-                        <td><input type="hidden" id="b2"></td>
-                        <td><input type="hidden" id="b3"></td>
-                    </tr>
-                    <tr>
-                        <td><input type="hidden" id="c1"></td>
-                        <td><input type="hidden" id="c2"></td>
-                        <td><input type="hidden" id="c3"></td>
-                    </tr>
+                    <?php
+                    for($id = 1; $id < 10; $id++){
+                        if( $id === 1 || $id === 4 || $id == 7){
+                           echo "<tr>";
+                        }
+
+                        echo "<td><input type='text' maxlength='1' name='$id' id='$id'></td>";
+
+//                        if (isset($_POST["submit"]) && !empty($_POST["$id"])){
+//
+//                        }
+
+                        if( $id === 3 || $id === 6 || $id == 9){
+                            echo "</tr>";
+                        }
+                    }
+                    ?>
                 </table>
+
+                <input type="submit" name="submit" value="Draw">
+
+                <?php
+                    $error = false;
+
+
+                ?>
             </form>
         </div>
     </div>
@@ -46,7 +76,10 @@
 <!--
     Set up AJAX and an Endpoint for sending game requests to.
     keep track of board, and refresh page every couple seconds to keep board up to date.
-    Keep some kind of game room ID for keeping track of the players facing one another
+    Keep some kind of game room ID for keeping track of the players facing one another.\
+
+
+    Check for a win condition or tie condition... somehow
 -->
 
 </body>
