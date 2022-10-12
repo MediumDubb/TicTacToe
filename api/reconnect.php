a6143f2806b526340ffe87bd1a9ce287eef5fcc5
MediumDubb <noah.whittington.1997@gmail.com>
2022-10-11 21:21:36 -0400

    added char to $row object for reconnect response

--- api/reconnect.php
+++ api/reconnect.php
@@ -7,7 +7,7 @@ require_once('../database/connect.php');
 
 $submission = $_REQUEST;
 
-$grab_room_users = $dbh->prepare("SELECT user_one_id, user_two_id FROM game_room WHERE id ='" . $submission['room_id']. "'");
+$grab_room_users = $dbh->prepare("SELECT user_one_id, user_two_id, table_data FROM game_room WHERE id ='" . $submission['room_id']. "'");
 
 if ( isset($submission['room_id']) && isset($submission['user_id'])) {
 
@@ -20,7 +20,7 @@ if ( isset($submission['room_id']) && isset($submission['user_id'])) {
     if ( $rows['user_one_id'] != null && $rows['user_one_id'] == $submission['user_id'] ){
         echo json_encode(['char' => 'x']);
     } elseif( $rows['user_one_id'] != null && $rows['user_two_id'] == $submission['user_id'] ) {
-        echo json_encode(['char' => 'o']);
+        echo json_encode( $rows += ['char' => 'o'] );
     } else {
         echo json_encode(['error' => 'Data specified not found']);
     }
